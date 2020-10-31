ea802d32dbc110e2053aa37d110132d1bae4c2e5
Noah Mah <noahmah@Noahs-MacBook-Pro.local>
2020-10-30 19:14:24 -0700

    commented out background image comps

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import MySwitch from "./comps/Switch";
 import MyAvatar from "./comps/Avatar";
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
+
 const App = () => {
   return (
-    <View>
-      <MyAvatar img="url(/girl.jpg)" />
+    <View style={styles.container}>
+      {/* <MyAvatar img="url(/girl.jpg)" /> */}
       <MySwitch />
     </View>
   );
--- comps/settings_section/index.js
+++ comps/settings_section/index.js
@@ -1,63 +1,63 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+// import React from "react";
+// import { View, Text, StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    position: "relative",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    width: 325,
-    height: 50,
+// const styles = StyleSheet.create({
+//   container: {
+//     position: "relative",
+//     flexDirection: "row",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     width: 325,
+//     height: 50,
 
-    margin: 50
-  },
-  text: {
-    fontWeight: "bold",
-    fontSize: "16",
-    paddingBottom: 5
-  },
-  line: {
-    position: "absolute",
-    width: 325,
-    height: 1,
-    backgroundColor: "#ececec",
-    bottom: 0
-  },
-  icon: {
-    width: 18,
-    height: 18,
-    position: "absolute",
-    left: 25
-  },
-  arrow: {
-    backgroundImage: "url(/arrow.svg)",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "contain",
-    width: 18,
-    height: 18,
-    position: "absolute",
-    right: 25
-  }
-});
-const MySection = ({ text, icon }) => {
-  const iconstyles = { backgroundImage: icon ? icon : "url(/)" };
+//     margin: 50
+//   },
+//   text: {
+//     fontWeight: "bold",
+//     fontSize: "16",
+//     paddingBottom: 5
+//   },
+//   line: {
+//     position: "absolute",
+//     width: 325,
+//     height: 1,
+//     backgroundColor: "#ececec",
+//     bottom: 0
+//   },
+//   icon: {
+//     width: 18,
+//     height: 18,
+//     position: "absolute",
+//     left: 25
+//   },
+//   arrow: {
+//     backgroundImage: "url(/arrow.svg)",
+//     backgroundRepeat: "no-repeat",
+//     backgroundSize: "contain",
+//     width: 18,
+//     height: 18,
+//     position: "absolute",
+//     right: 25
+//   }
+// });
+// const MySection = ({ text, icon }) => {
+//   const iconstyles = { backgroundImage: icon ? icon : "url(/)" };
 
-  return (
-    <View style={styles.container}>
-      <View style={[styles.icon, iconstyles]} />
+//   return (
+//     <View style={styles.container}>
+//       <View style={[styles.icon, iconstyles]} />
 
-      <Text style={styles.text}>{text}</Text>
+//       <Text style={styles.text}>{text}</Text>
 
-      <View style={styles.arrow} />
+//       <View style={styles.arrow} />
 
-      <View style={styles.line}></View>
-    </View>
-  );
-};
+//       <View style={styles.line}></View>
+//     </View>
+//   );
+// };
 
-MySection.defaultProps = {
-  text: "Default"
-};
+// MySection.defaultProps = {
+//   text: "Default"
+// };
 
-export default MySection;
+// export default MySection;
