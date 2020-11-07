d21bb6d1dd1b2b437f659c44d1a141fd03d75a2b
Justin Namoro <justinnamoro@JustinsMac.local>
2020-11-07 12:50:38 -0800

    button background props added

--- comps/button/index.js
+++ comps/button/index.js
@@ -3,35 +3,31 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FD8700",
+    textAlign: "center",
     height: 30,
     width: 108,
-    borderRadius: 5
+    padding: 10,
+    margin:10,
+    borderRadius:5
   },
   firstText: {
-    fontSize: 12,
     fontWeight: "bold",
-    color: "#f8f9fa"
+    color: "#FFF"
   }
 });
 
-const MyButton = ({ text, cancel }) => {
-  // No <div> in react-native
-  // Need to figure out how to prop the color... because we need to change the color for the cancel button
-  // const cancelStyles = {backgroundColor: cancel === "true" === "#333333"};
+const MyButton = ({ text, bgcolor }) => {
+  const newstyles = {backgroundColor: bgcolor ? bgcolor:"#FD8700"}
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={[styles.container, newstyles]}>
       <Text style={styles.firstText}>{text}</Text>
     </TouchableOpacity>
   );
 };
 
 MyButton.defaultProps = {
-  text: "Button",
-  cancel: "#fd8700"
+  text: "Button"
 };
 
 export default MyButton;
