986be7d97ec47252135970deb34acaa2131c6f0f
= <=>
2020-11-08 22:19:35 -0800

    Pulled from Main

--- App.js
+++ App.js
@@ -1,45 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-import { Button, Image, StyleSheet, Text, View } from "react-native";
-import MySwitch from "./comps/MySwitch";
-import Avatar from "./comps/Avatar";
-import MessageSection from "./comps/MessageSection";
-import EventSection from "./comps/EventSection";
-
-const App = () => {
-  return (
-    <View>
-      <Avatar img={require("../public/girl.jpg")} />
-
-      {/* code below is how to import a url picture*/}
-
-      {/* <Avatar
-        img={{ uri: "https://cdn.eso.org/images/thumb300y/eso1907a.jpg" }}
-      /> */}
-
-      <View>
-        <br />
-        <br />
-      </View>
-      <MySwitch />
-      <View>
-        <br />
-        <br />
-      </View>
-      <MessageSection name="Test" messageContent="Hello, this is a message" />
-      <br />
-      <br />
-      <EventSection
-        eventName="Event name"
-        eventTime="00:00"
-        eventLocation="Burnaby"
-        eventDesc="this is a description"
-      />
-      <br />
-      <br />
-=======
 import { View, StyleSheet } from "react-native";
-import MySwitch from "./comps/Switch";
+import MySwitch from "./comps/MySwitch";
 import SendtoInput from "./comps/sendtoinput";
 import PlayerAdminInput from "./comps/playeradmininput"
 import MsgInput from "./comps/msginput";
@@ -59,15 +20,14 @@ const styles = StyleSheet.create({
 const App = () => {
   return (
     <View style={styles.container}>
-      <PollPopUp />
+      {/* <PollPopUp /> */}
       {/* <MySection /> */}
       {/* <MyProgressBar /> */}
       {/* <SendtoInput /> */}
       {/* <PlayerAdminInput /> */}
       {/* <MsgInput /> */}
       {/* <MyAvatar img="url(/girl.jpg)" /> */}
-      {/* <MySwitch /> */}
->>>>>>> main
+      <MySwitch />
     </View>
   );
 };
