415b24be506170bce75f6f74be306f5fadf75388
= <=>
2020-11-09 01:31:24 -0800

    Completed finishplayerreg page and added logo

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import {StyleSheet, Text, View } from "react-native";
 import {NativeRouter, Route, Link} from "react-router-native";
 
 import AdminReg from "./pages/adminreg";
-import PlayerReg from "./pages/PlayerReg";
+import PlayerReg from "./pages/playerreg";
+import FinishPlayerReg from "./pages/finishplayerreg";
 
 const App = () => {
   return<View>
     {/* <AdminReg/> */}
-    <PlayerReg></PlayerReg>
+    {/* <PlayerReg></PlayerReg> */}
+    <FinishPlayerReg></FinishPlayerReg>
+
   </View>
 }
 
