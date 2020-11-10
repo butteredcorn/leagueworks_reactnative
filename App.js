6dff2f975be5a1899201bec44c390b9bd238203e
Justin Namoro <justinnamoro@JustinsMac.local>
2020-11-10 12:43:39 -0800

    fixing conflicts

--- App.js
+++ App.js
@@ -3,27 +3,34 @@ import {StyleSheet, Text, View } from "react-native";
 import {NativeRouter, Route, Link} from "react-router-native";
 
 
-// import Account from "./pages/account";
-// import AdminReg from "./pages/adminreg";
-// import PlayerReg from "./pages/PlayerReg";
-// import FinishPlayerReg from "./pages/finishplayerreg";
-// import AdminReg from "./pages/adminreg";
-// import AdminRegTeam from "./pages/adminregteam";
-// import AdminWaiver from "./pages/adminwaiver";
-// import Schedule from "./pages/schedule";
+
+import AdminReg from "./pages/adminreg";
+import PlayerReg from "./pages/PlayerReg";
+import FinishPlayerReg from "./pages/finishplayerreg";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import CreateEvent from "./pages/createevent";
 
 
 const App = () => {
   return<View>
-    {/* <AdminReg/> */}
-    {/* <PlayerReg></PlayerReg> */}
-    {/* <Account /> */}
-    {/* <FinishPlayerReg /></FinishPlayerReg> */}
-    {/* <AdminReg /> */}
-    {/* <AdminRegTeam /> */}
-    {/* <AdminWaiver /> */}
-    {/* <Schedule /> */}
+    <AdminReg/>
+    <PlayerReg></PlayerReg>
+    <FinishPlayerReg></FinishPlayerReg>
+
     <CreateEvent />
   </View>
 
